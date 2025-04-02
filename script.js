7adfd5afa3184f64c0b9c1c3211e30f302fcfde5
Isac Junio <isacsac.bh@gmail.com>
2025-04-02 00:02:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,21 +13,17 @@ function typeEffect() {
 
 typeEffect();
 
-
-
-// Botão volatar ao topo
-
-
-window.onscroll = function (){
-  let botao = document.getElementById("botaoTopo");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+// Botão de volta ao topo
+window.onscroll = function(){
+  let botao = document.getElementById("btnTopo")
+  if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
     botao.style.display = "block";
   }
   else {
     botao.style.display = "none";
   };
-};
+}
 
-function voltarTopo() {
-  window.scrollTo({top: 0, behavior: "smooth"});
+function voltarTopo(){
+  window.scroll({top:0, behavior: "smooth"});
 }
